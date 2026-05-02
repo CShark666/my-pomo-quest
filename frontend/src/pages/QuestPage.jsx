e744d72e848db2a1acfa4bc17e880a15e4a0c6bf
LeTaras <purplesloth.riario1337@gmail.com>
2026-05-02 03:36:25 +0300

    add params for dynamic url

--- frontend/src/pages/QuestPage.jsx
+++ frontend/src/pages/QuestPage.jsx
@@ -2,9 +2,12 @@ import { Sidebar } from "./Sidebar";
 import { QuestItem } from "../components/QuestItem";
 import { getQuest } from "../api";
 import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import "../styles/QuestPage.css";
 
 export function QuestPage() {
   const [quest, setQuest] = useState({});
+  const { id } = useParams();
 
   useEffect(() => {
     const fetchDate = async () => {
@@ -16,9 +19,11 @@ export function QuestPage() {
   });
   return (
     <>
-      QuestPage
       <Sidebar />
-      <QuestItem quest={quest}/>
+      <h1>QuestPage: {id}</h1>
+      <div className="quest-box">
+        <QuestItem quest={quest} />
+      </div>
     </>
   );
 }

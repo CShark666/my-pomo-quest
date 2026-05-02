9b4fc0a814da2f1925c28db7feed0b88711fef0f
LeTaras <purplesloth.riario1337@gmail.com>
2026-05-02 03:33:57 +0300

    add 404 page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,16 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import { HomePage } from "./pages/HomePage";
 import { CreateQuestPage } from "./pages/CreateQuestPage";
 import { QuestPage } from "./pages/QuestPage";
+import { NotFoundPage } from "./pages/NotFoundPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<HomePage />} />
-        <Route path="quest/create" element={<CreateQuestPage />} />
-        <Route path="quest" element={<QuestPage />} />
+        <Route path="/quest/create" element={<CreateQuestPage />} />
+        <Route path="/quest/:id" element={<QuestPage />} />
+        <Route path="*" element={<NotFoundPage />}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/NotFoundPage.jsx
@@ -0,0 +1,10 @@
+import { Sidebar } from "./Sidebar";
+
+export function NotFoundPage() {
+  return (
+    <>
+      <Sidebar />
+      <h1>404</h1>
+    </>
+  );
+}

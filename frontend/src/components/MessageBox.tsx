b0f020fc43b44c0a2a4b3aaf00bb01440675f935
LeTaras <purplesloth.riario1337@gmail.com>
2026-05-31 19:20:35 +0300

    styles for message box

--- frontend/src/components/MessageBox.tsx
+++ frontend/src/components/MessageBox.tsx
@@ -9,17 +9,19 @@ type MessageBoxProps = {
 }
 
 export function MessageBox({ text, buttons }: MessageBoxProps) {
-    return <div className="message-box">
-        <span>{text}</span>
-        {buttons?.map((button, index) => (
-            <div>
-                <button
-                    key={index}
-                    onClick={button.onClick}
-                >
-                    {button.text}
-                </button>
-            </div>
-        ))}
-    </div>
+    return (
+        <div className="grid justify-center items-center m-1.5 p-1.5">
+            <span className="text-2xl font-light">{text}</span>
+            {buttons?.map((button, index) => (
+                <div className="flex justify-center gap-1.5 m-1.5">
+                    <button
+                        key={index}
+                        className="btn"
+                        onClick={button.onClick}
+                    >
+                        {button.text}
+                    </button>
+                </div>
+            ))}
+        </div>)
 }

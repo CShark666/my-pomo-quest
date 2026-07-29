4990153ab1985ff1329dd7960af10276300020b7
LeTaras <purplesloth.riario1337@gmail.com>
2026-07-29 17:00:45 +0300

    refactor: UserPage now uses a mock backend

--- frontend/src/components/UserProfile.tsx
+++ frontend/src/components/UserProfile.tsx
@@ -1,7 +1,18 @@
-export function UserProfile() {
+import type { ClientUser } from "../types/types";
+import { logOutUser } from "../userApi";
+
+type UserProfileProps = {
+    user: ClientUser,
+    logOutAction: () => void
+}
+
+export function UserProfile({ user, logOutAction }: UserProfileProps) {
+    const handleLogUot = async () => {
+        await logOutUser();
+        logOutAction();
+    }
     return (
         <>
-
             <div className="flex-col bg-base-200 border-base-300 rounded-box w-xl border p-4 gap-3">
                 <div className="flex justify-end items-end">
                     <button className="btn btn-ghost">
@@ -11,18 +22,18 @@ export function UserProfile() {
 
                 <div className="flex w-full justify-between">
                     <div className="flex-1">
-                        <img src="../../public/user_pic.jpg" alt="profile-pic" className="rounded-full size-36" />
+                        <img src="/user_pic.jpg" alt="profile-pic" className="rounded-full size-36" />
                     </div>
 
                     <div className="flex flex-2 justify-between">
-                        <p>UserName</p>
-                        <p>Level</p>
+                        <p>{user.login}</p>
+                        <p>{user.level}</p>
                     </div>
 
                 </div>
 
                 <div className="flex justify-end items-end">
-                    <button className="btn btn-ghost">
+                    <button className="btn btn-ghost" onClick={handleLogUot}>
                         Log out
                     </button>
                 </div>
--- frontend/src/pages/UserPage.tsx
+++ frontend/src/pages/UserPage.tsx
@@ -1,13 +1,41 @@
+import { Suspense, use, useState } from "react";
+import { Navigate } from "react-router";
 import { Sidebar } from "./Sidebar";
 import { UserProfile } from "../components/UserProfile"
+import { LoadingSpinnerLabel } from "../components/Loading";
+import { getUser } from "../userApi";
+import type { ClientUser } from '../types/types';
+
+
+function UserPageContext({ initialUser }: { initialUser: Promise<ClientUser | null> }) {
+    const [user, setUser] = useState<ClientUser | null>(use(initialUser));
+
+    const logOutAction = () => {
+        setUser(null);
+    }
+
+    return (
+        <>
+            <div className="flex justify-center items-center h-screen">
+                {user ? (
+                    <UserProfile user={user} logOutAction={logOutAction}/>
+                ) : (
+                    <Navigate to="/authorization" />
+                )}
+            </div>
+        </>
+    );
+}
 
 export function UserPage() {
+    const initialUser = getUser();
+
     return (
         <>
             <Sidebar />
-            <div className="flex justify-center items-center h-screen">
-                <UserProfile />
-            </div>
+            <Suspense fallback={<LoadingSpinnerLabel />}>
+                <UserPageContext initialUser={initialUser} />
+            </Suspense>
         </>
     )
 }

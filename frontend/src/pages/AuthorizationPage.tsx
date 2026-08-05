e645657913f3f016af28fdf32088a1b24fd5e219
LeTaras <purplesloth.riario1337@gmail.com>
2026-08-05 16:45:04 +0300

    refactoring authorize page

--- frontend/src/pages/AuthorizationPage.tsx
+++ frontend/src/pages/AuthorizationPage.tsx
@@ -1,35 +1,32 @@
 import { Link, Navigate, Outlet } from "react-router";
 import { Suspense, useContext } from "react";
 import { LoadingSpinnerLabel } from "../components/Loading";
-import type { ClientUser } from "../types/types";
 import { UserContext } from "../contexts/UserContext";
 
-function AuthorizationPageContext({ user }: { user: ClientUser | null }) {
+function AuthorizationPageContext() {
     return (
         <>
             <div className="flex flex-col gap-2.5 justify-center items-center h-screen">
-                {user
-                    ? <Navigate to="/user" />
-                    : <>
-                        <samp>To get started, please
-                            <Link className="btn mr-1.5 ml-1.5" to="login">Log in</Link>
-                            or
-                            <Link className="btn mr-1.5 ml-1.5" to="signup">Sign up</Link>
-                        </samp>
-                        <Outlet />
-                    </>
-                }
+                <samp>To get started, please
+                    <Link className="btn mr-1.5 ml-1.5" to="login">Log in</Link>
+                    or
+                    <Link className="btn mr-1.5 ml-1.5" to="signup">Sign up</Link>
+                </samp>
+                <Outlet />
             </div>
         </>
     )
 }
 
 export function AuthorizationPage() {
-    const initialUser = useContext(UserContext);
+    const userContext = useContext(UserContext);
 
     return <>
         <Suspense fallback={<LoadingSpinnerLabel />}>
-            <AuthorizationPageContext user={initialUser.user} />
+            {userContext.user
+                ? <Navigate to="/" />
+                : <AuthorizationPageContext />
+            }
         </Suspense>
     </>
 }

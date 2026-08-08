6f9ae671abee433d927e550fce287dee0c069a4c
CShark666 <purplesloth.riario1337@gmail.com>
2026-08-08 22:43:39 +0300

    refactoring: fixed loading user problem after reload page

--- frontend/src/contexts/ContextProvider.tsx
+++ frontend/src/contexts/ContextProvider.tsx
@@ -2,21 +2,23 @@ import { useEffect, useState, type ReactNode } from "react";
 import { UserContext } from "./UserContext";
 import type { ClientUser } from "../types/types";
 import { getUser } from "../userApi";
+import { LoadingSpinnerLabel } from "../components/Loading";
 
 export function ContextProvider({ children }: { children: ReactNode }) {
     const [user, setUser] = useState<ClientUser | null>(null);
+    const [isUserReady, setIsUserReady] = useState<boolean>(false);
+
+
 
     useEffect(() => {
-        const fetchData = async () => {
-            const userData = await getUser();
-            setUser(userData)
-        }
-        fetchData();
+        getUser()
+            .then(setUser)
+            .then(() => setIsUserReady(true))
     }, [])
 
     return (
-        <UserContext.Provider value={{ user, setUser }}>
-            {children}
-        </UserContext.Provider>
+        isUserReady
+            ? <UserContext.Provider value={{ user, setUser }}> {children} </UserContext.Provider>
+            : <LoadingSpinnerLabel />
     )
 }

f634ab059d67ef4d7b7915b411a4bc0695944fc3
LeTaras <purplesloth.riario1337@gmail.com>
2026-09-04 14:02:59 +0300

    fix intervals animation bug

--- frontend/src/components/IntervalsBar.tsx
+++ frontend/src/components/IntervalsBar.tsx
@@ -26,8 +26,11 @@ export function IntervalsBar({
       {intervals.map((interval, i) => {
         return (
           <div key={i} className={`rounded-md ${isBreakMode ? "bg-break/20" : "bg-work/20"} text-white text-center grow shrink basis-15 min-w-8 max-w-24 h-10 relative overflow-hidden`}>
-            <div className={`rounded-md absolute bottom-0 w-full h-full ${isBreakMode ? "bg-break/50" : "bg-work/50"} animate-drain ${interval.active ? "solid border-2 border-active" : ""}`}
-              style={{ width: interval.percent + "%" }}>
+            <div className={`rounded-md absolute bottom-0 w-full h-full 
+              ${isBreakMode ? "bg-break/50" : "bg-work/50"} 
+              ${interval.active ? "solid border-2 border-active" : ""}`}
+              style={{ width: interval.percent + "%" }}
+            >
               {Math.floor(interval.percent) + "%"}
             </div>
           </div>
@@ -36,3 +39,10 @@ export function IntervalsBar({
     </div>
   );
 }
+
+
+// i === activeIntervalIdx
+//           ? timerPercent
+//           : i > activeIntervalIdx
+//             ? 0
+//             : 100,

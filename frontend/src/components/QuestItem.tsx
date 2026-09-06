aa55d85782b4dfce9559e65c3cf0e8945cc2514d
CShark666 <purplesloth.riario1337@gmail.com>
2026-09-07 00:37:55 +0300

    fix text size

--- frontend/src/components/QuestItem.tsx
+++ frontend/src/components/QuestItem.tsx
@@ -99,12 +99,12 @@ export function QuestItem({ quest, skipBreakAction, skipTransitionAction, isLoad
 function TotalTimeDesk({ time }: { time: string }) {
   return (
     <>
-      <div className={`flex shrink-0 justify-center items-center w-62.5 aspect-19/11 text-[#e7d5b3] text-[60px] font-bold relative overflow-hidden`}>
+      <div className={`flex shrink-0 justify-center items-center w-62.5 aspect-19/11 relative overflow-hidden`}>
         <img
           src={timeDesk}
           className="absolute inset-0 h-full w-full object-fill"
         />
-        <p className="absolute">
+        <p className="absolute text-[#e7d5b3] text-xl md:text-3xl lg:text-5xl font-bold">
           {time}
         </p>
       </div>

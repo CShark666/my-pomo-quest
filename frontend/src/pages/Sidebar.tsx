f28f2a43722f9808743abef68420bc2455e55b47
LeTaras <purplesloth.riario1337@gmail.com>
2026-09-25 16:59:19 +0300

    refactoring sidebar

--- frontend/src/pages/Sidebar.tsx
+++ frontend/src/pages/Sidebar.tsx
@@ -17,40 +17,69 @@ export function Sidebar() {
   return (
     <div className="drawer">
       <input id="my-drawer-1" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content fixed">
+
+      <div className="drawer-content">
+
         {/* Page content here */}
-        <label htmlFor="my-drawer-1">
-          <img src={icon} alt="sidebar" className="w-full max-w-12 cursor-pointer" />
-        </label>
+
+        <div className="fixed top-4 left-4 z-10">
+          <label htmlFor="my-drawer-1">
+            <img
+              src={icon}
+              alt="sidebar"
+              className="w-12 cursor-pointer"
+            />
+          </label>
+        </div>
 
       </div>
       <div className="drawer-side">
         <label htmlFor="my-drawer-1" aria-label="close sidebar" className="drawer-overlay"></label>
 
-        <ul className="menu bg-base-200 min-h-full w-60 p-4">
+        <aside className="bg-base-200 min-h-full w-60 flex flex-col">
 
-          <div className="flex justify-between outline-1 rounded-2xl p-1">
-            {user && (
-              <>
-                <div className="grid">
-                  <span>{user.name}</span>
-                  <span>Lvl: {user.level}</span>
-                  <span>Exp:{user.currentExperience}/240</span>
+          {/* User info */}
+          {user && (
+            <div className="p-4">
+              <Link to="user">Profile
+                <div className="flex justify-between rounded-2xl outline-1 p-2">
+                  <div className="grid">
+                    <span>{user.name}</span>
+                    <span>Lvl: {user.level}</span>
+                    <span>
+                      Exp: {user.currentExperience}/240
+                    </span>
+                  </div>
+
+                  {user.streak > 0 && (
+                    <span>
+                      Streak: {user.streak}
+                    </span>
+                  )}
                 </div>
-                <div>
-                  <span>{user!.streak > 0 && `streaks: ${user.streak}`}</span>
-                </div>
-              </>
-            )}
+              </Link>
+            </div>
+          )}
+
+          <ul className="menu p-4 h-full">
+
+            {/* Sidebar content here */}
+            <li>Pages:</li>
+            <li><Link to="/">Home page</Link></li>
+            <li><Link to="quest">Pomo-Quest</Link></li>
+            <li><Link to="questsHistory">Quests history</Link></li>
+          </ul>
+
+          {/* Logout */}
+          <div className="mt-auto p-4">
+            <button className="btn btn-ghost"
+              onClick={logOutAction}
+            >
+              Log out
+            </button>
           </div>
 
-          {/* Sidebar content here */}
-          <li><Link to="user">Profile</Link></li>
-          <li><Link to="/">Home page</Link></li>
-          <li><Link to="quest">Pomo-Quest</Link></li>
-          <li><Link to="questsHistory">Quests history</Link></li>
-          <li><button className="btn btn-ghost" onClick={logOutAction}>Log out</button></li>
-        </ul>
+        </aside>
       </div>
     </div>
   )
